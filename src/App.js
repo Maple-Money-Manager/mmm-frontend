bbdfe4eeb66131b0723a987316808c0192d2453b
vallq <vallq@outlook.com>
2020-08-09 13:19:28 +0800

    [#52][Val] add page state management to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
+import EmptyPage from "./pages/EmptyPage";
 
 function App() {
-  return <HomePage />;
+  const [currentPage, setCurrentPage] = React.useState();
+
+  const renderPage = (pageName) => {
+    switch (pageName) {
+      case "Home":
+        return <HomePage />;
+      case "Lorem":
+        return <EmptyPage />;
+      default:
+        return <HomePage />;
+    }
+  };
+  return (
+    <React.Fragment>
+      <NavBar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {renderPage(currentPage)}
+    </React.Fragment>
+  );
 }
 
 export default App;
