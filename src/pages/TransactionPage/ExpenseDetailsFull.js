33955076e7068d0c018f6ee7cd6679866eec3a76
Valerie Lui <vallq@users.noreply.github.com>
2020-09-13 02:13:29 +0800

    add editable fields and state

--- src/pages/TransactionPage/ExpenseDetailsFull.js
+++ src/pages/TransactionPage/ExpenseDetailsFull.js
@@ -2,28 +2,41 @@ import React from "react";
 import {
   Grid,
   Card,
+  CardActions,
   CardContent,
   CardHeader,
+  FormControl,
+  IconButton,
   Typography,
   Button,
+  OutlinedInput,
+  InputAdornment,
 } from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
 import { useParams, Link } from "react-router-dom";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import "date-fns";
+import DateFnsUtils from "@date-io/date-fns";
 
-export default function ExpenseDetailsFull({ expenseList }) {
+export const ExpenseDetailsFull = ({ expenseList }) => {
+  const [editState, setEditState] = React.useState(false);
   let { uniqueKey } = useParams();
   const position = uniqueKey.charAt(uniqueKey.length - 1);
   if (uniqueKey.length < 0) {
     return <Grid />;
   }
   return (
-    <Grid container direction="column" spacing={3}>
-      <Grid item>
-        <Card variant="outlined">
-          <CardHeader
-            title="Transaction Details"
-            subheader={expenseList[position].date.toLocaleString()}
-          />
-          <CardContent>
+    <Grid>
+      <Card variant="outlined">
+        <CardHeader
+          title="Transaction Details"
+          subheader={expenseList[position].date.toLocaleString()}
+        />
+        <CardContent>
+          {!editState && (
             <Typography
               data-testid="expense-list"
               variant="body2"
@@ -34,14 +47,55 @@ export default function ExpenseDetailsFull({ expenseList }) {
               Amount: ${expenseList[position].expense} <br />
               Date: {expenseList[position].date.toLocaleString()} <br />
             </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item>
-        <Button variant="outlined" color="primary" component={Link} to={"/"}>
-          Back
-        </Button>
-      </Grid>
+          )}
+          {editState && (
+            <>
+              <FormControl variant="outlined">
+                <OutlinedInput
+                  type="number"
+                  id="edit-amount-field"
+                  inputProps={{ "aria-label": "editExpenseInput" }}
+                  startAdornment={
+                    <InputAdornment position="start">$</InputAdornment>
+                  }
+                />
+              </FormControl>
+              <FormControl variant="outlined">
+                <OutlinedInput
+                  type="text"
+                  id="edit-category-field"
+                  inputProps={{ "aria-label": "editCategoryInput" }}
+                  defaultValue={expenseList[position].category}
+                />
+              </FormControl>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  margin="normal"
+                  id="date-picker-dialog"
+                  label="Date picker dialog"
+                  format="MM/dd/yyyy"
+                  inputProps={{ "aria-label": "editDateInput" }}
+                  KeyboardButtonProps={{
+                    "aria-label": "change date",
+                  }}
+                />
+              </MuiPickersUtilsProvider>
+            </>
+          )}
+        </CardContent>
+        <CardActions>
+          <IconButton
+            onClick={() =>
+              editState ? setEditState(false) : setEditState(true)
+            }
+          >
+            <EditIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+      <Button variant="outlined" color="primary" component={Link} to={"/"}>
+        Back
+      </Button>
     </Grid>
   );
-}
+};
